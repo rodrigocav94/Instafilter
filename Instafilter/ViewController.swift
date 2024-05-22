94904d220b1ba014e4f0a969989cbd9910ba4022
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-05-22 16:13:54 -0300

    Added function to apply filters, added action sheet to set different filters.

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -6,16 +6,23 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
     var currentImage: UIImage!
     
+    var context: CIContext! // Core Image component that handles rendering. Creating a context is computationally expensive so we don't want to keep doing it.
+    var currentFilter: CIFilter! // Whatever filter the user has activated.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Instafilter"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
     
     @objc func importPicture() {
@@ -29,15 +36,68 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         guard let image = info[.editedImage] as? UIImage else { return }
         dismiss(animated: true)
         currentImage = image
+        
+        let beginImage = CIImage(image: currentImage) //  Core Image equivalent of UIImage.
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey) // set our currentImage property as the input image for the currentFilter. (?)
+        applyProcessing()
     }
 
     @IBAction func changeFilter(_ sender: Any) {
+        let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func setFilter(action: UIAlertAction) {
+        guard let currentImage, let actionTitle = action.title else { return }
+        
+        currentFilter = CIFilter(name: actionTitle)
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
     }
     
     @IBAction func save(_ sender: Any) {
     }
     
     @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        guard let image = currentFilter.outputImage else { return }
+        let inputKeys  = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) { // Not all filters support changing intensity.
+            currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) // uses the value of intensity slider to set the kCIInputIntensityKey value of our current Core Image filter. 0 means "no effect", 1 means "fully sepia."
+            
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey)
+        }
+
+        if inputKeys.contains(kCIInputCenterKey) {
+            currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey)
+        }
+        
+        if let cgImage = context.createCGImage(image, from: image.extent) { //  Creates a new data type called CGImage from the output image of the current filter. By using image.extent, specify that you want to render all of it.
+            let processedImage = UIImage(cgImage: cgImage) // creates a new UIImage from the CGImage,
+            imageView.image = processedImage
+        }
     }
 }
 
